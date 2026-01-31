fab872d3ce8b3d2fc3f2fde513257b1b4731f3a2
Amit Bashan <amitxbashan@gmail.com>
2026-01-31 04:13:12 +0200

    feat: add parse template function with validation

--- lib/textfsm.ex
+++ lib/textfsm.ex
@@ -1,5 +1,16 @@
 defmodule TextFSM do
-  @moduledoc """
-  Documentation for `Textfsm`.
-  """
+  alias __MODULE__.Template
+
+  def parse(template) do
+    with {:ok, [template], _, _, _, _} <- Template.template(template),
+         [] <- Template.Validator.validate(template) do
+      {:ok, template}
+    else
+      {:error, reason, rest, context, line, byte_offset} ->
+        {:error, reason, rest, context, line, byte_offset}
+
+      validation_errors ->
+        {:error, validation_errors}
+    end
+  end
 end
