b956d35c59552e8d26e144d7cbf0f33344a95823
Amit Bashan <amitxbashan@gmail.com>
2026-01-31 02:57:52 +0200

    feat: add lifting of TextFSM template to its struct

--- lib/textfsm/template.ex
+++ lib/textfsm/template.ex
@@ -2,18 +2,26 @@ defmodule TextFSM.Template do
   @enforce_keys [:value_definitions, :states]
   defstruct [:value_definitions, :states]
 
+  alias __MODULE__.{ValueDefinition, State}
+
   @type t() :: %__MODULE__{
-          value_definitions: [__MODULE__.ValueDefinition.t()],
-          states: [__MODULE__.State.t()]
+          value_definitions: [ValueDefinition.t()],
+          states: [State.t()]
         }
+
+  @spec value_names(t()) :: [String.t()]
+  def value_names(%__MODULE__{value_definitions: value_definitions}) do
+    value_definitions
+    |> Enum.map(& &1.name)
+  end
 
   import NimbleParsec
 
   newlines = parsec({TextFSM.ParserHelpers, :newlines})
 
-  value_definition = parsec({__MODULE__.ValueDefinition, :value_definition})
+  value_definition = parsec({ValueDefinition, :value_definition})
 
-  state = parsec({__MODULE__.State, :state})
+  state = parsec({State, :state})
 
   value_definitions =
     times(concat(value_definition, newlines), min: 1)
@@ -24,5 +32,27 @@ defmodule TextFSM.Template do
   defparsec(
     :template,
     concat(value_definitions, states)
+    |> post_traverse({:lift, []})
   )
+
+  defp lift(rest, args, context, _position, _offset) do
+    {value_definitions, states} =
+      Enum.split_with(
+        args,
+        fn
+          %ValueDefinition{} ->
+            true
+
+          %State{} ->
+            false
+        end
+      )
+
+    template = %__MODULE__{
+      value_definitions: value_definitions,
+      states: states
+    }
+
+    {rest, [template], context}
+  end
 end
